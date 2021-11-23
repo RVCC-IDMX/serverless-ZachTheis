6baecd4b3950754e31bbfdbfe1393caa9ad1115f
ZachTheis <70988581+ZachTheis@users.noreply.github.com>
2021-11-23 15:59:01 -0500

    Replace fetch with axios

--- serverless/jokes.js
+++ serverless/jokes.js
@@ -1,20 +1,26 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
+
+/*
+ https://axios-http.com/docs/res_schema
+*/
 
 // eslint-disable-next-line no-unused-vars
 exports.handler = async (event, context) => {
   const url = 'https://icanhazdadjoke.com/';
   try {
-    const jokeStream = await fetch(url, {
+    const jokeStream = await axios.get(url, {
       headers: {
         Accept: 'application/json',
+        'User-Agent': 'axios 0.21.1',
       },
     });
-    const jsonJoke = await jokeStream.json();
+
     return {
       statusCode: 200,
-      body: JSON.stringify(jsonJoke),
+      body: JSON.stringify(jokeStream.data),
     };
   } catch (err) {
+    console.log(err);
     return { statusCode: 422, body: err.stack };
   }
 };
